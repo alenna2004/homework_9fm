e6962d1773b39bbe67c0d06c09119022fd0706fc
alenna2004 <55256093+alenna2004@users.noreply.github.com>
2019-09-27 12:08:31 +0300

    Update test_Mymatrix.py

--- test_Mymatrix.py
+++ test_Mymatrix.py
@@ -7,7 +7,7 @@ def test_repr():
 def test_size():
     s = [[1,2,3],[4,5,6]]
     mat = MyMatrix(s)
-    assert(mat.size() == '(2,3)')
+    assert(mat.size() == (2,3))
 def test_get_data():
     s = [[1,2,3],[4,5,6]]
     mat = MyMatrix(s)
@@ -15,50 +15,68 @@ def test_get_data():
 def test_flip_up_down():
     s = [[1,2,3],[4,5,6]]
     mat = MyMatrix(s)
-    assert(mat.flip_up_down() == [[4,5,6],[1,2,3]])
+    fud = mat.flip_up_down()
+    assert(fud.get_data() == [[4,5,6],[1,2,3]])
 def test_flip_left_right():
     s = [[1,2,3],[4,5,6]]
     mat = MyMatrix(s)
-    assert(mat.flip_left_right() == [[3,2,1],[6,5,4]])
+    flr = mat.flip_left_right()
+    assert(flr.get_data() == [[3,2,1],[6,5,4]])
 def test_flipped_up_down():
     s = [[1,2,3],[4,5,6]]
     mat = MyMatrix(s)
     flud = mat.flipped_up_down()
-    assert(flud == [[4,5,6],[1,2,3]])
+    assert(flud.get_data() == [[4,5,6],[1,2,3]])
     assert(mat.get_data()== [[1,2,3],[4,5,6]])
 def test_flipped_left_right():
     s = [[1,2,3],[4,5,6]]
     mat = MyMatrix(s)
     fllr = mat.flipped_left_right()
-    assert(fllr== [[3,2,1],[6,5,4]])
+    assert(fllr.get_data()== [[3,2,1],[6,5,4]])
     assert(mat.get_data()== [[1,2,3],[4,5,6]])
 def test_transpose():
     s = [[1,2,3],[4,5,6]]
     mat = MyMatrix(s)
-    assert(mat.transpose()== [[1,4],[2,5],[3,6]])
+    tra = mat.transpose()
+    assert(tra.get_data()== [[1,4],[2,5],[3,6]])
 def test_transposed():
     s = [[1,2,3],[4,5,6]]
     mat = MyMatrix(s)
-    tr = mat.transposed()
-    assert(tr== [[1,4],[2,5],[3,6]])
+    tr = mat.transposed()  
+    assert(tr.get_data()== [[1,4],[2,5],[3,6]])
     assert(mat.get_data()== [[1,2,3],[4,5,6]])
 def test__add__():
-    other = [[3,5,6],[9,4,6]]
     s = [[1,2,3],[4,5,6]]
-    mat = MyMatrix(s)
-    assert(mat.__add__(other)== [[4,7,9],[13,9,12]])
-def test__sub__():
     other = [[1,1,2],[2,2,3]]
-    s = [[1,2,3],[4,5,6]]
     mat = MyMatrix(s)
-    assert(mat.__sub__(other)== [[0,1,1],[2,3,3]])
+    oth = MyMatrix(other)
+    ads = mat + oth
+    assert(ads.get_data()== [[2, 3, 5], [6, 7, 9]])
+    assert(mat.get_data()== [[1,2,3],[4,5,6]])
+    assert(oth.get_data()== [[1,1,2],[2,2,3]])
+def test__sub__():
+    s = [[1,2,3],[4,5,6]]
+    other = [[1,1,2],[2,2,3]]
+    mat = MyMatrix(s)
+    oth = MyMatrix(other)
+    subs = mat - oth
+    assert(subs.get_data()== [[0,1,1],[2,3,3]])
+    assert(mat.get_data()== [[1,2,3],[4,5,6]])
+    assert(oth.get_data()== [[1,1,2],[2,2,3]])
 def test_iadd():
     other = [[1,1,2],[2,2,3]]
     s = [[1,2,3],[4,5,6]]
+    oth = MyMatrix(other)
     mat = MyMatrix(s)
-    assert(mat.iadd(other)== [[2, 3, 5],[6,7,9]])
+    mat += oth
+    assert(mat.get_data()== [[2, 3, 5],[6,7,9]])
 def test_isub():
     other = [[1,1,2],[2,2,3]]
     s = [[1,2,3],[4,5,6]]
     mat = MyMatrix(s)
-    assert(mat.isub(other)== [[0,1,1],[2,3,3]])
+    oth = MyMatrix(other)
+    mat -= oth
+    assert(mat.get_data()== [[0,1,1],[2,3,3]])
+def test_empt():
+    empt_m = MyMatrix([])
+    assert(empt_m.size() == (0,0))
