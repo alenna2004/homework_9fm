b20e90ba80a3402e3daabaaeb650c4fb1a4b519e
alenna2004 <55256093+alenna2004@users.noreply.github.com>
2020-01-30 21:47:54 +0300

    Create google_sheets.py

---
+++ google_sheets.py
@@ -0,0 +1,50 @@
+# 1 и 2(уже сдано)
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+gc = gspread.authorize(creds)
+dir(gc)
+sh = gc.open('conduit 9')
+worksheet = sh.worksheet('9')
+lis = worksheet.get_all_values()
+with open("out.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(lis)
+df = read_csv("out.csv")
+lis
+
+# 3
+def create_table_without_duplicate(sheet_name: str, row_number: int, col_number: int):
+    sh = gc.open('conduit 9')
+    try:
+        worksheet = sh.add_worksheet(title=sheet_name, rows=row_number, cols= col_number)
+    except:
+        sh.del_worksheet(sh.worksheet(sheet_name))
+        return create_table_without_duplicate(sheet_name, row_number,col_number)
+        
+create_table_without_duplicate('Nikolaeva',30,20)
+
+# 4
+def copy_student_marks(surname: str, row_number: int):
+    if row_number < 2 or row_number > 40:
+        raise Exception("Вы ввели неадекватный номер строки")
+    else:
+        sh = gc.open('conduit 9')
+        from_worksheet = sh.worksheet('9')
+        to_worksheet = sh.worksheet('Nikolaeva')
+        r1 = from_worksheet.row_values(1)
+        r2 = from_worksheet.row_values(2)
+        for i in range(len(r1)):
+            to_worksheet.update_cell(1, i+1, r1[i])
+        for i in range(len(r2)):
+            to_worksheet.update_cell(2, i+1, r2[i])
+        lis = from_worksheet.get_all_values()
+        i = 1
+        while surname not in lis[i][0]:
+            i = i + 1
+            if i > len(lis):
+                print("Этого чеовека не существует")
+                break
+        marks = lis[i]
+        for i in range(len(marks)):
+            to_worksheet.update_cell(row_number, i+1, marks[i])
+copy_student_marks('Николаева', 3)
